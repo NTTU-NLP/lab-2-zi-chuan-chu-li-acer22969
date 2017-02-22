7df1334cf369ab1073890ffa226a36e7401ceb9f
acer22969 <acer22969@gmail.com>
2017-02-22 20:22:33 +0800

    lab2

---
+++ lab2/lab2/lab2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader(@"..\..\..\..\..\Data\Input.txt"))
+            {
+                while (sr.Peek() != -1)
+                {
+                    string line = sr.ReadLine();
+                    foreach (var ch in line)
+                    {
+                        Console.Write(ch);
+                        if (ch == '.' || ch == '?' || ch == '!')
+                            Console.WriteLine();
+                    }
+                }
+            }
+            Console.Read();
+        }
+    }
+}
